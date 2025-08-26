1fa2d23d77eb9912a4da8c12611ea8aec2404a00
Zabodashka <igorsulga6@gmail.com>
2025-08-26 20:44:40 +0300

    Update test_tests.py

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -6,19 +6,23 @@ from app.main import check_password
     "Pass@word1",
     "A1$bcdefg",
     "A1$bcdefghijklmno",
-    "Valid#123",
+    "Valid#123"
 ])
 def test_valid_passwords(password):
     assert check_password(password) is True
 
 
 @pytest.mark.parametrize("password", [
-    "short1!",        # меньше 8 символов
-    "toolongpassword1$!",  # больше 16 символов
-    "NoSpecialChar1", # нет спецсимвола
-    "nospecialchar1", # нет заглавной буквы и спецсимвола
-    "NoDigit!@",      # нет цифры
-    "InvalidChar%1A", # недопустимый символ %
+    "qwerty",
+    "Str@ng",
+    "password1",
+    "PASSWORD@",
+    "Pass_word",
+    "12345678",
+    "Pass@word1 ",
+    "Pässwörd1$",
+    "Пароль@123",
+    "A1$bcdefghijklmnop",
 ])
 def test_invalid_passwords(password):
     assert check_password(password) is False
