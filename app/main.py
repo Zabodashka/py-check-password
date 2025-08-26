5475ac86e15986ba517d3e94e0a162c6ef8031a5
Zabodashka <igorsulga6@gmail.com>
2025-08-26 21:27:42 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,19 +6,21 @@ def check_password(password: str) -> bool:
         return False
 
     allowed_specials = "$@#&!-_;"
-    allowed_chars = set(
-        string.ascii_letters + string.digits + allowed_specials
-    )
+    allowed_chars = set(string.ascii_letters + string.digits + allowed_specials)
 
+    # Проверка на наличие только допустимых символов
     if not all(c in allowed_chars for c in password):
         return False
 
+    # Проверка на наличие заглавной буквы
     if not any(c.isupper() for c in password):
         return False
 
+    # Проверка на наличие цифры
     if not any(c.isdigit() for c in password):
         return False
 
+    # Проверка на наличие специального символа
     if not any(c in allowed_specials for c in password):
         return False
 
