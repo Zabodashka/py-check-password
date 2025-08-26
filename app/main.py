08675ac82d2fb5ad39ecbf494b31a97b389dc58c
Zabodashka <igorsulga6@gmail.com>
2025-08-26 21:36:51 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,17 +5,17 @@ def check_password(password: str) -> bool:
     if not (8 <= len(password) <= 16):
         return False
 
-    allowed_specials = "$@#&!-_;"
+    allowed_specials = "$@#&!-_"
     allowed_chars = set(
         string.ascii_letters + string.digits + allowed_specials
     )
 
-    for char in password:
-        if char not in allowed_chars:
+    for character in password:
+        if character not in allowed_chars:
             return False
 
-    has_upper = any(char.isupper() for char in password)
-    has_digit = any(char.isdigit() for char in password)
-    has_special = any(char in allowed_specials for char in password)
+    has_upper = any(character.isupper() for character in password)
+    has_digit = any(character.isdigit() for character in password)
+    has_special = any(character in allowed_specials for character in password)
 
     return has_upper and has_digit and has_special
