511178bb84ee82a3d51bb6a48759cfd58d8484f2
Igor Shulga <igorsulga6@gmail.com>
2025-08-26 20:49:12 +0300

    Fix: add tests to app folder for GitHub Actions

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,28 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize("password", [
+    "Pass@word1",
+    "A1$bcdefg",
+    "A1$bcdefghijklmno",
+    "Valid#123",
+])
+def test_valid_passwords(password):
+    assert check_password(password) is True
+
+
+@pytest.mark.parametrize("password", [
+    "qwerty",
+    "Str@ng",
+    "password1",
+    "PASSWORD@",
+    "Pass_word",
+    "12345678",
+    "Pass@word1 ",
+    "Pässwörd1$",
+    "Пароль@123",
+    "A1$bcdefghijklmnop",
+])
+def test_invalid_passwords(password):
+    assert check_password(password) is False
