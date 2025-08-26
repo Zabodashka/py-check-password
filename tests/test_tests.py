7470424288a0bc0d82e661707c3f429001e09ed9
Zabodashka <igorsulga6@gmail.com>
2025-08-26 21:43:48 +0300

    Update test_tests.py

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -5,7 +5,7 @@ from app.main import check_password
 @pytest.mark.parametrize("password", [
     "Pass@word1",
     "A1$bcdefg",
-    "A1$bcdefghijklmno",
+    "A1$bcdefghijklmn",
     "Valid#123"
 ])
 def test_valid_passwords(password):
