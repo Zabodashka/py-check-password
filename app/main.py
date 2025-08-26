e761958fda12e7b561b9c9a042f67280e5d2ecdb
Zabodashka <igorsulga6@gmail.com>
2025-08-26 20:34:10 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,9 +6,7 @@ def check_password(password: str) -> bool:
         return False
 
     allowed_specials = "$@#&!-_;"
-    allowed_chars = set(
-        string.ascii_letters + string.digits + allowed_specials
-    )
+    allowed_chars = set(string.ascii_letters + string.digits + allowed_specials)
 
     if not all(c in allowed_chars for c in password):
         return False
